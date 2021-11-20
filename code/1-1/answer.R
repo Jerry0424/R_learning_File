3be8347c46ea06b3c011296bacc2fe85832ce257
0424jerry <410821319@gms.ndhu.edu.tw>
2021-11-20 21:48:56 +0800

    Unfinish
    WA

--- code/1-1/answer.R
+++ code/1-1/answer.R
@@ -1,1 +1,41 @@
-1-1
+# 1-1
+
+prime <- function(n){
+	if(n <= 2){
+		return (2)
+	}
+	
+	sq <- round(sqrt(n))
+	
+	if (sq <= 2){
+		sq = 2
+	}
+	
+	tmp <- prime(sq)
+	ans <- tmp
+	print(n)
+	print((sq + 1))
+	print(tmp)
+	for(i in (sq + 1) : n ){
+		for (j in tmp){
+			print(j)
+			if(i < j){
+				ans <- append(ans,i)
+				break
+			}
+			if(!(i%%j) & (i!=j)){break}
+			else{
+				ans <- append(ans,i)
+			}
+			
+		}
+		
+		
+		
+		
+	}
+	return (ans)
+		
+}
+unique(prime(20))
+
